4c8799f263444368e808541242d8ba13fdf395d9
mahdi <mahdi.seidnejad10@gmail.com>
2025-03-29 14:27:16 +0330

    add reqly

---
+++ frontend/web/assets/b5498fd3/AppAsset.php
@@ -0,0 +1,20 @@
+<?php
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $sourcePath = '@frontend/assets'; // مسیر درست
+    public $css = [
+        'css/bootstrap.min.css', // مسیر صحیح
+    ];
+    public $js = [
+        'js/bootstrap.bundle.min.js', // مسیر صحیح
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapPluginAsset',
+    ];
+}
+
